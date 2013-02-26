5773e0a9d3ee4ffbdbfc3d7a96437f01b9ac1bf1
Torsten Raudssus <torsten@raudss.us>
2013-02-26 03:00:02 +0100

    Added Alien PluginBundle, added tests

--- lib/Dist/Zilla/Plugin/Alien.pm
+++ lib/Dist/Zilla/Plugin/Alien.pm
@@ -1,5 +1,127 @@
-use strict;
-use warnings;
 package Dist::Zilla::Plugin::Alien;
+# ABSTRACT: Use Alien::Base with Dist::Zilla
 
+use Moose;
+extends 'Dist::Zilla::Plugin::ModuleBuild';
+with 'Dist::Zilla::Role::PrereqSource';
+
+use URI;
+
+=head1 DESCRIPTION
+
+  name = Alien-myapp
+
+  [Alien]
+  repo = http://ffmpeg.org/releases
+  name = myapp
+  pattern_prefix = myapp-
+  pattern_version = ([\d\.]+)
+  pattern_suffix = \.tar\.gz
+  pattern = myapp-([\d\.]+)\.tar\.gz
+
+=cut
+
+has name => (
+	isa => 'Str',
+	is => 'rw',
+	lazy_build => 1,
+);
+sub _build_name {
+	my ( $self ) = @_;
+	my $name = $self->zilla->name;
+	$name =~ s/^Alien-//g;
+	return $name;
+}
+
+has repo => (
+	isa => 'Str',
+	is => 'rw',
+	required => 1,
+);
+
+has repo_uri => (
+	isa => 'URI',
+	is => 'rw',
+	lazy_build => 1,
+);
+sub _build_repo_uri {
+	my ( $self ) = @_;
+	URI->new($self->repo);
+}
+
+has pattern_prefix => (
+	isa => 'Str',
+	is => 'rw',
+	lazy_build => 1,
+);
+sub _build_pattern_prefix {
+	my ( $self ) = @_;
+	return $self->name.'-';
+}
+
+has pattern_version => (
+	isa => 'Str',
+	is => 'rw',
+	lazy_build => 1,
+);
+sub _build_pattern_version { '([\d\.]+)' }
+
+has pattern_suffix => (
+	isa => 'Str',
+	is => 'rw',
+	lazy_build => 1,
+);
+sub _build_pattern_suffix { '\\.tar\\.gz' }
+
+has pattern => (
+	isa => 'Str',
+	is => 'rw',
+	lazy_build => 1,
+);
+sub _build_pattern {
+	my ( $self ) = @_;
+	join("",
+		$self->pattern_prefix.
+		$self->pattern_version.
+		$self->pattern_suffix
+	);
+}
+
+sub register_prereqs {
+	my ( $self ) = @_;
+	$self->zilla->register_prereqs({
+			type  => 'requires',
+			phase => 'configure',
+		},
+		'Alien::Base' => '0.002',
+	);
+	$self->zilla->register_prereqs({
+			type  => 'requires',
+			phase => 'runtime',
+		},
+		'Alien::Base' => '0.002',
+	);
+}
+
+has "+mb_class" => (
+	default => 'Alien::Base::ModuleBuild',
+);
+
+around module_build_args => sub {
+	my ($orig, $self, @args) = @_;
+	my $pattern = $self->pattern;
+	return {
+		%{ $self->$orig(@args) },
+		alien_name => $self->name,
+		alien_repository => {
+			protocol => $self->repo_uri->scheme,
+			host => $self->repo_uri->host_port,
+			location => $self->repo_uri->path,
+			pattern => qr/^$pattern$/,
+		},
+	};
+};
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
 1;
---
+++ lib/Dist/Zilla/PluginBundle/Alien.pm
@@ -0,0 +1,34 @@
+package Dist::Zilla::PluginBundle::Alien;
+# ABSTRACT: Dist::Zilla::PluginBundle::Basic for Alien
+
+use Moose;
+use Moose::Autobox;
+use Dist::Zilla;
+with 'Dist::Zilla::Role::PluginBundle::Easy';
+
+=head1 DESCRIPTION
+
+This plugin bundle allows to use L<Dist::Zilla::Plugin::Alien> together
+with L<Dist::Zilla::PluginBundle::Basic>.
+
+  name = Alien-ffmpeg
+
+  [@Alien]
+  repo = http://ffmpeg.org/releases
+
+=cut
+
+use Dist::Zilla::PluginBundle::Basic;
+
+sub configure {
+  my ($self) = @_;
+
+  $self->add_bundle('Filter' => {
+    -bundle => '@Basic',
+    -remove => ['MakeMaker'],
+  });
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
